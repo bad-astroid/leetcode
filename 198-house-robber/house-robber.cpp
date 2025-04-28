0a0620d347f56c9bc7f44ac02e8ad1d340cb5337
bad-astroid <a7hammoudeh@gmail.com>
2025-04-28 19:32:20 +0200

    Time: 0 ms (100.00%) | Memory: 10.7 MB (45.04%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int N = nums.size(), K = 500 * N;
-        vector<vector<int>> ans(N + 1, vector<int>(K + 1, 0));
-        ans[0][0] = 1;
-        int robbed = 0;
-        for(int i = 1; i <= N; ++i) {
-            for(int k = 1; k <= K; ++k) {
-                ans[i][k] |= ans[i-1][k];
-                if(nums[i - 1] == k) {
-                    ans[i][k] = 1;
-                }
-                if(nums[i - 1] <= k && i >= 2) {
-                    ans[i][k] |= ans[i - 2][k - nums[i - 1]];
-                }
-
-                if(ans[i][k]) {
-                    robbed = max(robbed, k);
-                }
-            }
+        int n = nums.size();
+        if(n == 1){
+            return nums[0];
+        }
+        vector<int> ans(n);
+        ans[0] = nums[0];
+        ans[1] = max(nums[1], ans[0]);
+        for(int i = 2; i < n; ++i) {
+            ans[i] = max(ans[i - 1], ans[i - 2] + nums[i]);
         }
 
-
-        // for(int i = 1; i <= N; ++i) {
-        //     for(int k = 1; k <= 5; ++k) {
-        //         printf("ans[%d][%d] = %d\n", i, k, ans[i][k]);
-        //     }
-        // }
-
-        return robbed;        
+        return ans[n - 1];        
     }
 };
