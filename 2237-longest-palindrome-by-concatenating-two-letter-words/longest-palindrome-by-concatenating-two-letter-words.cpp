ad37e67cf42fcd5c53449299374da26a92bd7224
bad-astroid <a7hammoudeh@gmail.com>
2025-05-16 02:33:06 +0200

    Time: 88 ms (55.44%) | Memory: 172.2 MB (20.77%) - LeetSync

---
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string inverse(string word) {
+        swap(word[0], word[1]);
+        return word;
+    }
+    int longestPalindrome(vector<string>& words) {
+        unordered_map<string, int> fr, visited;
+        for (auto word : words) {
+            fr[word]++;
+        }
+        int ret = 0;
+        bool thereIsMiddle = false;
+        for (auto word : words) {
+            if(visited[word]) continue;
+
+            visited[word] = 1;
+            visited[inverse(word)] = 1;
+
+            if (word[0] == word[1]){
+                if (fr[word]&1) {
+                    ret += 2 * (fr[word] - 1);
+                    if(!thereIsMiddle) {
+                        ret += 2;
+                        thereIsMiddle = true;
+                    }
+                    continue;
+                }
+                ret += 2 * fr[word];
+                continue;
+            }
+            ret += 4 * min(fr[word], fr[inverse(word)]);
+        }
+
+        return ret;
+    }
+};
