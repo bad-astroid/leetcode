6a8a39098ad96d67143aef0d30288dea81c47c0c
bad-astroid <a7hammoudeh@gmail.com>
2025-05-17 23:32:36 +0200

    Time: 0 ms (100.00%) | Memory: 17.5 MB (67.61%) - LeetSync

---
+++ 1176-design-a-leaderboard/design-a-leaderboard.cpp
@@ -0,0 +1,45 @@
+class Leaderboard {
+public:
+    multiset<int> scores;
+    map<int,int> players;
+    Leaderboard() { }
+    
+    void addScore(int playerId, int score) {
+        if (!players.contains(playerId)) {
+            scores.insert(score);
+            players[playerId] = score;
+            return;
+        }
+
+        auto scoreIt = scores.find(players[playerId]);
+        int newScore = *scoreIt + score;
+        scores.erase(scoreIt);
+        scores.insert(newScore);
+        players[playerId] = newScore;
+    }
+    
+    int top(int K) {
+        auto rit = scores.rbegin();
+        int ret = 0;
+        while(K--) {
+            ret += *rit;
+            rit++;
+        }
+
+        return ret;
+    }
+    
+    void reset(int playerId) {
+        auto scoreIt = scores.find(players[playerId]);
+        scores.erase(scoreIt);
+        players.erase(playerId);
+    }
+};
+
+/**
+ * Your Leaderboard object will be instantiated and called as such:
+ * Leaderboard* obj = new Leaderboard();
+ * obj->addScore(playerId,score);
+ * int param_2 = obj->top(K);
+ * obj->reset(playerId);
+ */
