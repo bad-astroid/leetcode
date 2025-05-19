a6f8fd1785c25ebd5bc045cff5f00d1d717ca1bb
bad-astroid <a7hammoudeh@gmail.com>
2025-05-19 23:40:11 +0200

    Time: 184 ms (53.23%) | Memory: 186.8 MB (25.39%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
     struct Node{
-        vector<int> nxt = vector<int>(10, -1);
-        int fr = 0;
+        vector<Node*> nxt = vector<Node*>(10, nullptr);
     };
-    vector<Node> trie = vector<Node>(1, Node());
+    Node* root = new Node() ;
     void insert(string s) {
-        int u = 0;
+        Node* u = root;
         for(int i = 0; i < s.size(); ++i) {
-            if(trie[u].nxt[s[i] - '0'] == -1) {
-                trie[u].nxt[s[i] - '0'] = trie.size();
-                trie.push_back(Node());
+            if(u->nxt[s[i] - '0'] == nullptr) {
+                u->nxt[s[i] - '0'] = new Node();
             }
 
-            u = trie[u].nxt[s[i] - '0'];
-            trie[u].fr++;
+            u = u->nxt[s[i] - '0'];
         }
     }
     int find(string s) {
-        int u = 0;
+        Node* u = root;
         for(int i = 0; i < s.size(); ++i) {
-            if(trie[u].nxt[s[i] - '0'] == -1) {
+            if(u->nxt[s[i] - '0'] == nullptr) {
                 return i;
             }
-            u = trie[u].nxt[s[i] - '0'];
+            u = u->nxt[s[i] - '0'];
         }
+
         return s.size();
     }
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
         for(auto a : arr1) {
             insert(to_string(a));
         }
-        // to_string(num).length()
+
         int ret = 0;
         for(auto a : arr2) {
             string s = to_string(a);
