fcc1340ba4a51f976cd653b6e750af3bf5da9905
bad-astroid <a7hammoudeh@gmail.com>
2025-05-19 23:33:31 +0200

    Time: 205 ms (48.00%) | Memory: 186.3 MB (26.93%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
-    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        unordered_map<int,bool> prefixExists;
-        for(auto a : arr1) {
-            while(a) {
-                prefixExists[a] = true;
-                a /= 10;
+    struct Node{
+        vector<int> nxt = vector<int>(10, -1);
+        int fr = 0;
+    };
+    vector<Node> trie = vector<Node>(1, Node());
+    void insert(string s) {
+        int u = 0;
+        for(int i = 0; i < s.size(); ++i) {
+            if(trie[u].nxt[s[i] - '0'] == -1) {
+                trie[u].nxt[s[i] - '0'] = trie.size();
+                trie.push_back(Node());
             }
+
+            u = trie[u].nxt[s[i] - '0'];
+            trie[u].fr++;
+        }
+    }
+    int find(string s) {
+        int u = 0;
+        for(int i = 0; i < s.size(); ++i) {
+            if(trie[u].nxt[s[i] - '0'] == -1) {
+                return i;
+            }
+            u = trie[u].nxt[s[i] - '0'];
+        }
+        return s.size();
+    }
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        for(auto a : arr1) {
+            insert(to_string(a));
         }
         // to_string(num).length()
         int ret = 0;
         for(auto a : arr2) {
-            while(a) {
-                if(prefixExists.contains(a)) {
-                    ret = max(ret, ((int)log10(a) + 1));
-                }
-
-                a /= 10;
-            }
+            string s = to_string(a);
+            ret = max(ret, find(s));
         }  
 
         return ret;      
