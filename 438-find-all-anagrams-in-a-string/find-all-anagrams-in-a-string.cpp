b2c28b7ce7c0c42f4a6e38100658b849e6ae8d10
bad-astroid <a7hammoudeh@gmail.com>
2025-05-21 00:37:54 +0200

    Time: 3 ms (82.02%) | Memory: 11.6 MB (65.47%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -9,30 +9,19 @@ public:
             ++pFr[p[i] - 'a'];
         }
         int cnt = 0;
-        for(int i = 0; i < (int)p.size(); ++i) {
-            ++sFr[s[i] - 'a'];
-            if(sFr[s[i] - 'a'] == pFr[s[i] - 'a']) {
-                cnt += pFr[s[i] - 'a'];
+
+        for(int r = 0, l = 0; r < s.size(); ++r){
+            if(r >= p.size()) {
+                --sFr[s[l] - 'a'];
+                if(sFr[s[l] - 'a'] == pFr[s[l] - 'a']) {
+                    cnt +=  pFr[s[l] - 'a'];
+                }
+                if(sFr[s[l] - 'a'] == pFr[s[l] - 'a'] - 1) {
+                    cnt -=  pFr[s[l] - 'a'];
+                }
+                ++l;
             }
 
-            if(sFr[s[i] - 'a'] == pFr[s[i] - 'a'] + 1) {
-                cnt -= pFr[s[i] - 'a'];
-            }
-        }
-
-        if(cnt == p.size()) {
-            ret.push_back(0);
-        }
-
-        for(int r = (int)p.size(), l = 0; r < s.size(); ++r){
-            --sFr[s[l] - 'a'];
-            if(sFr[s[l] - 'a'] == pFr[s[l] - 'a']) {
-                cnt +=  pFr[s[l] - 'a'];
-            }
-            if(sFr[s[l] - 'a'] == pFr[s[l] - 'a'] - 1) {
-                cnt -=  pFr[s[l] - 'a'];
-            }
-            ++l;
 
             ++sFr[s[r] - 'a'];
             if(sFr[s[r] - 'a'] == pFr[s[r] - 'a']) {
