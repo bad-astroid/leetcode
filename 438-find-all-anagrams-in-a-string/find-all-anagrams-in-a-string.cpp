94edd01a3f30179b3128e2fdf0c4ad7aff7adcbe
bad-astroid <a7hammoudeh@gmail.com>
2025-05-21 00:03:46 +0200

    Time: 489 ms (7.22%) | Memory: 11.6 MB (42.30%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        cout << s.size() << ' ' << p.size() << endl;
+        if(p.size() > s.size()) {
+            return vector<int>(0);
+        }
+        vector<int> pFr(26, 0), sFr(26), ret;
+        for(int i = 0; i < p.size(); ++i) {
+            ++pFr[p[i] - 'a'];
+        }
+        int cnt = 0;
+        for(int i = 0; i < (int)p.size(); ++i) {
+            if(++sFr[s[i] - 'a'] == pFr[s[i] - 'a']) {
+                cnt += pFr[s[i] - 'a'];
+            }
+        }
+
+        if(cnt == p.size()) {
+            ret.push_back(0);
+        }
+
+        for(int r = (int)p.size(), l = 0; r < s.size(); ++r){
+            if(--sFr[s[l] - 'a'] == pFr[s[l] - 'a'] - 1) {
+                cnt -=  pFr[s[l] - 'a'];
+            }
+            ++l;
+
+            if(++sFr[s[r] - 'a'] == pFr[s[r] - 'a']) {
+                cnt += pFr[s[r] - 'a'];
+            }
+
+            cout << l << ' ' << cnt << endl;
+            if(cnt == p.size()) {
+                ret.push_back(l);
+            }
+        }
+
+
+
+        return ret;
+    }
+};
