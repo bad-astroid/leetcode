2ad171e1aea9c1d956871ae5744ec28e317a4b5d
bad-astroid <a7hammoudeh@gmail.com>
2025-05-21 22:17:44 +0200

    Time: 0 ms (100.00%) | Memory: 10.9 MB (70.20%) - LeetSync

--- 352-data-stream-as-disjoint-intervals/data-stream-as-disjoint-intervals.cpp
+++ 352-data-stream-as-disjoint-intervals/data-stream-as-disjoint-intervals.cpp
@@ -3,13 +3,12 @@ public:
     set<pair<int,int>> ranges;
     SummaryRanges() {
         ranges.insert(make_pair(-2, -2));
-        ranges.insert(make_pair(1e4+2, 1e4+2));
     }
     
     void addNum(int value) {
         auto range = make_pair(value, value);
         auto it = ranges.upper_bound(range);
-        if(value >= it->first && value <= it->second) {
+        if(it != ranges.end() && value >= it->first && value <= it->second) {
             return;
         }
         it--;
@@ -19,17 +18,13 @@ public:
         
         if(it->second == value - 1) {
             range.first = it->first;
-            ranges.erase(it++);
+            it = ranges.erase(it);
         }
         else{
             it++;
         }
 
-        if(value >= it->first && value <= it->second) {
-            return;
-        }
-
-        if(it->first == value + 1) {
+        if(it != ranges.end() && it->first == value + 1) {
             range.second = it->second;
             ranges.erase(it);
         }
@@ -39,7 +34,7 @@ public:
     
     vector<vector<int>> getIntervals() {
         vector<vector<int>> ret;
-        for(auto it = next(ranges.begin()); next(it) != ranges.end(); it++) {
+        for(auto it = next(ranges.begin()); it != ranges.end(); it++) {
             ret.push_back({it->first, it->second});
         }
 
