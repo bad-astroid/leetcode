472aa52ddf1ca9ab10607b186c58f26223af3559
bad-astroid <a7hammoudeh@gmail.com>
2025-06-13 00:04:17 +0200

    Time: 1124 ms (5.10%) | Memory: 105.4 MB (13.27%) - LeetSync

--- 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.cpp
+++ 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.cpp
@@ -1,45 +1,19 @@
 class Solution {
 public:
-    int compressNums(vector<vector<int>>& flowers, vector<int>& people) {
-        vector<int> nums;
-        for(auto flower : flowers) {
-            nums.push_back(flower[0]);
-            nums.push_back(flower[1]);
-            nums.push_back(flower[1]+1);
-        }
-        for(auto person : people) {
-            nums.push_back(person);
-        }
-        sort(nums.begin(), nums.end());
-
-        for(auto& flower : flowers) {
-            for(auto& time : flower) {
-                int at = lower_bound(nums.begin(), nums.end(), time) - nums.begin();
-                time = at;
-            }
-        }
-        for(auto& person : people) {
-            int at = lower_bound(nums.begin(), nums.end(), person) - nums.begin();
-            person = at;
-        }
-        return nums.size();
-    }
     vector<int> fullBloomFlowers(vector<vector<int>> flowers, vector<int> people) {
-        vector<int> ret;
-        int N = compressNums(flowers, people);
-        vector<int> fullBloomFlowersAt(N, 0);
+        vector<int> ret, starts, ends;
         for(auto flower : flowers) {
-            fullBloomFlowersAt[flower[0]] += 1;
-            fullBloomFlowersAt[flower[1]+1] -= 1;
+            starts.push_back(flower[0]);
+            ends.push_back(flower[1]);
         }
-        for(int i = 1; i < N; ++i) {
-            fullBloomFlowersAt[i] += fullBloomFlowersAt[i - 1];
+        sort(starts.begin(), starts.end());
+        sort(ends.begin(), ends.end());
+        for(auto person : people) {
+            int startBeforeCount = upper_bound(starts.begin(), starts.end(), person) - starts.begin();
+            int endBeforeCount = lower_bound(ends.begin(), ends.end(), person) - ends.begin();
+            ret.push_back(startBeforeCount - endBeforeCount);
+            cout << person << ' ' << startBeforeCount << ' ' << endBeforeCount << endl;
         }
-
-        for(auto personTime : people) {
-            ret.push_back(fullBloomFlowersAt[personTime]);
-        }
-        
         return ret;
     }
 };
