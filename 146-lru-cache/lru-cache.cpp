51c2c97f519037f9d4ec75891cc129b892c73004
bad-astroid <a7hammoudeh@gmail.com>
2025-06-12 16:16:09 +0200

    Time: 126 ms (13.29%) | Memory: 191 MB (5.18%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,7 +1,7 @@
 class LRUCache {
 public:
     list<pair<int,int>> cache;
-    map<int, list<pair<int,int>>::iterator> mp;
+    unordered_map<int, list<pair<int,int>>::iterator> mp;
     int capacity;
     LRUCache(int _capacity) {
         capacity = _capacity;
