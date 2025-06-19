e31188e213e687a67295e5d78929d6ccdcf3dfe8
bad-astroid <a7hammoudeh@gmail.com>
2025-06-19 18:40:50 +0200

    Time: 17 ms (42.67%) | Memory: 216 MB (23.73%) - LeetSync

--- 2217-step-by-step-directions-from-a-binary-tree-node-to-another/step-by-step-directions-from-a-binary-tree-node-to-another.cpp
+++ 2217-step-by-step-directions-from-a-binary-tree-node-to-another/step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -11,64 +11,52 @@
  */
 class Solution {
 public:
-    bool traverse(TreeNode* u, vector<pair<int,char>>& path, int dest, char direction = '*') {
-        if(u == nullptr) {
-            return false;
-        }
-
+    bool dfs(TreeNode* u, int dest, vector<TreeNode*>& ancestors) {
+        if(u == nullptr) return false;
+        // cout << u->val << endl;
         if(u->val == dest) {
-            path.push_back(make_pair(u->val, direction));
+            ancestors.push_back(u);
             return true;
         }
 
-        if (traverse(u->left, path, dest, 'L')) {
-            path.push_back(make_pair(u->val, direction));
+        if(dfs(u->left, dest, ancestors)) {
+            ancestors.push_back(u);
             return true;
         }
 
-        if (traverse(u->right, path, dest, 'R')) {
-            path.push_back(make_pair(u->val, direction));
+        if(dfs(u->right, dest, ancestors)) {
+            ancestors.push_back(u);
             return true;
         }
 
         return false;
     }
     string getDirections(TreeNode* root, int startValue, int destValue) {
-        vector<pair<int,char>> pathToStart, pathToDest;
-        traverse(root, pathToStart, startValue);
-        traverse(root, pathToDest, destValue);
+        string ret = "";
+        vector<TreeNode*> pathToStart, pathToDest;
+        dfs(root, startValue, pathToStart);
+        dfs(root, destValue, pathToDest);
+
         reverse(pathToStart.begin(), pathToStart.end());
         reverse(pathToDest.begin(), pathToDest.end());
 
-        // for(auto u : pathToStart) {
-        //     cout << u.first << " ";
-        // }
-        // puts("");
-
         // for(auto u : pathToDest) {
-        //     cout << u.first << " ";
+        //     cout << u->val << endl;
         // }
-        // puts("");
-
         int lca = 0;
-        for(int i = 0; i < min(pathToStart.size(), pathToDest.size()); ++i) {
-            if(pathToStart[i].first == pathToDest[i].first) {
-                lca = i + 1;
-            }
+        for(int i = 0; i < min((int)pathToDest.size(), (int)pathToStart.size()); ++i) {
+            if(pathToDest[i]->val != pathToStart[i]->val) break;
+            lca = i;
         }
-        // if(lca + 1 == min(pathToStart.size(), pathToDest.size())) {
-        //     lca++;
-        // }
-        string ans = "";
-
-        for(int i = lca; i < pathToStart.size(); ++i) {
-            ans += 'U';
+        for(int i = lca+1; i < pathToStart.size(); ++i) {
+            ret += 'U';
         }
 
-        for(int i = lca; i < pathToDest.size(); ++i) {
-            ans += pathToDest[i].second;
+        for(int i = lca+1; i < pathToDest.size(); ++i) {
+            if(pathToDest[i-1]->left == pathToDest[i]) ret += 'L';
+            else ret += 'R';
         }
-
-        return ans;
+        
+        return ret;
     }
 };
