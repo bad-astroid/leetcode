375b982e55d286decbc34cbcfeaa3a913d3dd579
bad-astroid <a7hammoudeh@gmail.com>
2025-06-22 19:18:01 +0200

    Time: 100 ms (24.67%) | Memory: 93.5 MB (46.39%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
@@ -1,48 +1,33 @@
 class Solution {
 public:
-    struct Job{
-        int start;
-        int finish;
-        int val;
-
-        Job(int _start, int _finish, int _val) {
-            start = _start;
-            finish = _finish;
-            val = _val;
+    struct Job {
+        int left, right, profit;
+        Job(int _left, int _right, int _profit) {
+            left = _left;
+            right = _right;
+            profit = _profit;
         }
-    
-        bool operator<(const Job& other) const {
-            return finish < other.finish;
+
+        bool operator< (Job &job) {
+            return right < job.right;
         }
     };
-
-    int findEarliestJob(const vector<Job>& jobs, int i) {
-        int lo = 0, hi = i - 1, j = 0;
-        while(lo <= hi) {
-            int mid = (lo + hi) / 2;
-            if(jobs[i].start >= jobs[mid].finish) {
-                j = mid;
-                lo = mid + 1;
-            }
-            else {
-                hi = mid - 1;
-            }
-        }
-        return j;
-    }
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-        int n = startTime.size();
-        vector<Job> jobs(1, Job(-1,-1,0));
-        for(int i = 0; i < n; ++i) {
+        vector<Job> jobs;
+        jobs.push_back(Job(0, 0, 0));
+        for(int i = 0; i < startTime.size(); ++i) {
             jobs.push_back(Job(startTime[i], endTime[i], profit[i]));
         }
         sort(jobs.begin(), jobs.end());
+        map<int, int> dp;
+        dp[0] = 0;
+        for(int i = 1; i < jobs.size(); ++i) {
+            auto it = dp.upper_bound(jobs[i].left);
+            it--;
 
-        vector<int> ans(n+1);
-        for(int i = 1; i <= n; ++i) {
-            int j = findEarliestJob(jobs, i);
-            ans[i] = max(ans[i-1], ans[j] + jobs[i].val);
+            dp[jobs[i].right] = max(jobs[i].profit + it->second, dp[jobs[i-1].right]);
         }
-        return ans[n];
+
+        return dp[jobs.back().right];
     }
 };
