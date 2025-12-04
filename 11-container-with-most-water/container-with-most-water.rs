4b8322e01596ed24949bb1ce51fbee617519be78
bad-astroid <a7hammoudeh@gmail.com>
2025-12-05 00:53:10 +0100

    Time: 0 ms (100.00%) | Memory: 3.1 MB (5.69%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.rs
+++ 11-container-with-most-water/container-with-most-water.rs
@@ -1,12 +1,10 @@
-use std::cmp;
-
 impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
         let mut ans = 0;
         let mut l = 0;
         let mut r = height.len()-1;
         while l < r {
-            ans = cmp::max(ans, (r - l) as i32 * (cmp::min(height[l], height[r])));
+            ans = ans.max((r - l) as i32 * height[l].min(height[r]));
             if height[l] < height[r] {
                 l += 1;
             } else {
