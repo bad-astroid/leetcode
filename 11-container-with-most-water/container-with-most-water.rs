a53969517307029c2af70c4eb879990509eabd67
bad-astroid <a7hammoudeh@gmail.com>
2025-12-08 11:05:49 +0100

    Time: 10 ms (13.03%) | Memory: 3.4 MB (2.11%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.rs
+++ 11-container-with-most-water/container-with-most-water.rs
@@ -1,16 +1,28 @@
 impl Solution {
-    pub fn max_area(height: Vec<i32>) -> i32 {
-        let mut ans = 0;
-        let mut l = 0;
-        let mut r = height.len()-1;
-        while l < r {
-            ans = ans.max((r - l) as i32 * height[l].min(height[r]));
-            if height[l] < height[r] {
-                l += 1;
-            } else {
-                r -= 1;
+    fn max_area_left_to_right(height: & Vec<i32>) -> i32 {
+        let mut maxes = vec![];
+        let mut ret = 0;
+
+        for (i, h) in height.iter().enumerate() {
+            if maxes.is_empty() {
+                maxes.push((i, h));
+            }
+
+            let leftmost_greater_index = maxes.partition_point(|&(_, value)| value < h);
+            if leftmost_greater_index < maxes.len() {
+                let (index, _) = maxes[leftmost_greater_index];
+                ret = ret.max((i - index) as i32 * h);
+            }
+
+            if let Some(&(_, max)) = maxes.last() && max < h{
+                maxes.push((i, h));    
             }
         }
-        return ans;
+        return ret;
+    }
+
+    pub fn max_area(height: Vec<i32>) -> i32 {
+        let reversed_height = height.iter().rev().cloned().collect();
+        return Self::max_area_left_to_right(& height).max(Self::max_area_left_to_right(& reversed_height));
     }
 }
