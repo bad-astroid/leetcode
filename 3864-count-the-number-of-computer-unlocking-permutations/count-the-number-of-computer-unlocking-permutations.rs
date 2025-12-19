00b0d6ef8c4fa01e5b7be55d68373a9547986c78
bad-astroid <a7hammoudeh@gmail.com>
2025-12-19 16:11:45 +0100

    Time: 3 ms (19.06%) | Memory: 3.7 MB (37.69%) - LeetSync

---
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    const M: i64 = 1_000_000_007;
+    pub fn mult(a: i64, b: i64) -> i32 {
+        return (((a % Self::M) * (b % Self::M)) % Self::M) as i32;
+    }
+    pub fn count_permutations(complexity: Vec<i32>) -> i32 {
+        let mut ret = 1;
+        for (i, &c) in complexity.iter().enumerate() {
+            if i == 0 { continue; }
+            ret = Self::mult(i as i64, ret as i64);
+            if c <= complexity[0] { return 0; }
+        }
+
+    return ret;
+    }
+}
