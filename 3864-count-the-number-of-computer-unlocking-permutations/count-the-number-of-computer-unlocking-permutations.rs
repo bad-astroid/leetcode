6647e1e8905a70a331fb5a8d145c255289ae1426
bad-astroid <a7hammoudeh@gmail.com>
2025-12-19 16:21:16 +0100

    Time: 1 ms (28.27%) | Memory: 3.6 MB (84.80%) - LeetSync

--- 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.rs
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.rs
@@ -1,8 +1,5 @@
 impl Solution {
-    const M: i64 = 1_000_000_007;
-
     pub fn count_permutations(complexity: Vec<i32>) -> i32 {
-        let mut ret: i64 = 1;
         let n = complexity.len();
 
         for i in 1..n {
@@ -11,8 +8,10 @@ impl Solution {
             }
         }
 
+        let mut ret: i64 = 1;
+        let mod_val: i64 = 1000000007;
         for i in 1..n {
-            ret = (ret * i as i64) % Self::M;
+            ret = (ret * i as i64) % mod_val;
         }
 
         return ret as i32;
